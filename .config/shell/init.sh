e84dc270315d059ef7000e39e0c4d4f97d22717b
InSuperposition <f.galloway@gmail.com>
2024-06-08 03:46:30 +0300

    Update init.sh

--- .config/shell/init.sh
+++ .config/shell/init.sh
@@ -13,4 +13,8 @@
 . ~/.config/starship/init.sh
 . ~/.config/shell/alias.sh
 
+# https://wiki.archlinux.org/title/XDG_Base_Directory
 export XDG_CONFIG_HOME=~/.config
+export XDG_CACHE_HOME=~/.cache
+export XDG_DATA_HOME=~/.local/share
+export XDG_STATE_HOME=~/.local/state
