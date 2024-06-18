3f413a38c7196e401655582fa0c06ac90eca59fd
InSuperposition <f.galloway@gmail.com>
2024-06-19 00:19:43 +0300

    add fnm

--- .config/init.sh
+++ .config/init.sh
@@ -19,6 +19,7 @@ export GRAALVM_HOME="/Library/Java/JavaVirtualMachines/$GRAALVM_VERSION/Contents
 export JAVA_HOME="$GRAALVM_HOME" # "$ANDROID_JVM"
 export JAVA_BIN_PATH="$JAVA_HOME/bin"
 
+. $XDG_CONFIG_HOME/fnm/init.sh
 . $XDG_CONFIG_HOME/pyenv/init.sh
 . $XDG_CONFIG_HOME/starship/init.sh
 . $XDG_CONFIG_HOME/alias.sh
