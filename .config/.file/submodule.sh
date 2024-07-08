4435e86cc9c6c09f50b291ef7401672a37682dae
InSuperposition <f.galloway@gmail.com>
2024-07-08 19:43:46 +0300

    add submodules

--- .config/.file/submodule.sh
+++ .config/.file/submodule.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 . $XDG_CONFIG_HOME/android/init.sh
+. $XDG_CONFIG_HOME/cargo/init.sh
 . $XDG_CONFIG_HOME/fnm/init.sh
 . $XDG_CONFIG_HOME/gnugpg/init.sh
 . $XDG_CONFIG_HOME/homebrew/init.sh
@@ -7,4 +8,5 @@
 . $XDG_CONFIG_HOME/minikube/init.sh
 . $XDG_CONFIG_HOME/pnpm/init.sh
 . $XDG_CONFIG_HOME/pyenv/init.sh
+. $XDG_CONFIG_HOME/rustup/init.sh
 . $XDG_CONFIG_HOME/starship/init.sh
