bb40da33403d39c842207e87cf68974502da5684
InSuperposition <f.galloway@gmail.com>
2024-07-08 20:41:42 +0300

    add kube submodule

--- .config/.file/alias.sh
+++ .config/.file/alias.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-
-alias k='kubectl'
 alias ls='ls --color=always'
--- .config/.file/submodule.sh
+++ .config/.file/submodule.sh
@@ -5,6 +5,7 @@
 . $XDG_CONFIG_HOME/gnugpg/init.sh
 . $XDG_CONFIG_HOME/homebrew/init.sh
 . $XDG_CONFIG_HOME/java/init.sh
+. $XDG_CONFIG_HOME/kube/init.sh
 . $XDG_CONFIG_HOME/minikube/init.sh
 . $XDG_CONFIG_HOME/pnpm/init.sh
 . $XDG_CONFIG_HOME/pyenv/init.sh
