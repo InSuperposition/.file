5ef5f7147921cc76c4693c38375e2010140d4ed2
InSuperposition <f.galloway@gmail.com>
2025-06-08 10:11:54 +0300

    add npm submodule

--- .config/.file/submodule.sh
+++ .config/.file/submodule.sh
@@ -7,6 +7,7 @@
 . $XDG_CONFIG_HOME/homebrew/init.sh
 . $XDG_CONFIG_HOME/kube/init.sh
 . $XDG_CONFIG_HOME/minikube/init.sh
+. $XDG_CONFIG_HOME/npm/init.sh
 . $XDG_CONFIG_HOME/pnpm/init.sh
 . $XDG_CONFIG_HOME/pyenv/init.sh
 . $XDG_CONFIG_HOME/rustup/init.sh
