e51e6269e7468fb173e02d3215bb6f1e02a80a55
InSuperposition <f.galloway@gmail.com>
2025-06-21 18:18:22 +0300

    update zi

--- .config/.file/init.sh
+++ .config/.file/init.sh
@@ -5,9 +5,10 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
-export XDG_BIN_HOME=$HOME/.local/bin
 
-export PATH="$PATH:$XDG_BIN_HOME"
+LOCAL_BIN_HOME=$HOME/.local/bin
+
+export PATH="$PATH:$LOCAL_BIN_HOME"
 
 # XDG_RUNTIME_DIR - https://wiki.gentoo.org/wiki/XDG/Base_Directories
 if [ -z "${XDG_RUNTIME_DIR}" ]; then
