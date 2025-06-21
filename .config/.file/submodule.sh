8f51c820d0848c523002e2c6402429cb67ece717
InSuperposition <f.galloway@gmail.com>
2025-06-21 17:29:58 +0300

    remove npm

--- .config/.file/submodule.sh
+++ .config/.file/submodule.sh
@@ -5,7 +5,6 @@
 . $XDG_CONFIG_HOME/homebrew/init.sh
 . $XDG_CONFIG_HOME/kaggle/init.sh
 . $XDG_CONFIG_HOME/mise/init.sh
-. $XDG_CONFIG_HOME/npm/init.sh
 . $XDG_CONFIG_HOME/pnpm/init.sh
 . $XDG_CONFIG_HOME/rustup/init.sh
 . $XDG_CONFIG_HOME/starship/init.sh
