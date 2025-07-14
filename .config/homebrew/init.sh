b7fb141d3c5323305abacaafcaf221c21f52fd63
InSuperposition <f.galloway@gmail.com>
2025-07-14 11:16:00 +0300

    feat: Update Brewfile and init.sh for improved package management and add claude tool to config

--- .config/homebrew/init.sh
+++ .config/homebrew/init.sh
@@ -5,3 +5,8 @@ export HOMEBREW_BUNDLE_FILE=$XDG_CONFIG_HOME/homebrew/Brewfile
 
 alias brew-lock="brew bundle dump --force && brew bundle install --no-upgrade"
 alias brew-init="brew bundle dump --force && brew bundle install"
+
+if type brew &>/dev/null; then
+  HOMEBREW_PREFIX=$(brew --prefix)
+  export PATH="$HOMEBREW_PREFIX/sbin:$PATH"
+fi
