cefcf9a4f4137e04364ab9a77b78c85fbcc43c89
InSuperposition <f.galloway@gmail.com>
2025-09-24 15:45:58 +0300

    add link to pkg-config repository in init.sh for reference

--- .config/homebrew/init.sh
+++ .config/homebrew/init.sh
@@ -10,5 +10,6 @@ if type brew &>/dev/null; then
   HOMEBREW_PREFIX=$(brew --prefix)
   export PATH="$HOMEBREW_PREFIX/sbin:$PATH"
   # For building pkg-config, which is needed for some things like postgresql
+  # https://github.com/pkgconf/pkgconf
   export PKG_CONFIG_PATH="$HOMEBREW_PREFIX/bin/pkg-config:$(brew --prefix icu4c)/lib/pkgconfig:$(brew --prefix curl)/lib/pkgconfig:$(brew --prefix zlib)/lib/pkgconfig"
 fi
