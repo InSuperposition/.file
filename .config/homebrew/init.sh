ab8b278c68355e014079b5ad4b3958eba9eb0321
InSuperposition <f.galloway@gmail.com>
2026-01-13 13:41:39 +0200

    fix: update settings.json to add ralph-loop plugin and improve Brewfile with additional packages

--- .config/homebrew/init.sh
+++ .config/homebrew/init.sh
@@ -8,9 +8,11 @@ alias brew_init="brew bundle install"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# deduplicate path
+typeset -U PATH path
+
 if type brew &>/dev/null; then
   HOMEBREW_PREFIX=$(brew --prefix)
-  export PATH="$HOMEBREW_PREFIX/sbin:$HOMEBREW_PREFIX/bin:$PATH"
   # For building pkg-config, which is needed for some things like postgresql
   # https://github.com/pkgconf/pkgconf
   export PKG_CONFIG_PATH="$HOMEBREW_PREFIX/bin/pkg-config:$(brew --prefix icu4c)/lib/pkgconfig:$(brew --prefix curl)/lib/pkgconfig:$(brew --prefix zlib)/lib/pkgconfig"
